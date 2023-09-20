1d9ed85327710d5c7de639f09bea28b5ba53eec1
DelroyGayle <delroygayle01@gmail.com>
2023-09-20 13:12:53 +0000

    Test POST request with Hello message

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -30,8 +30,10 @@ def about_member(member_name):
     return "<h1>" + member["name"] + "</h1>"
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print("Hello! Is Anybody there?")    
     return render_template("contact.html", page_title="Contact")
 
 
